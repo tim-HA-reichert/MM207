cc9c73da227f06bf3747460f59502b287a6e6a55
Tim <tim.reichert111@gmail.com>
2025-01-07 10:14:33 +0100

    Working server code.

--- myfirst.js
+++ myfirst.js
@@ -1,11 +1,6 @@
-// server.mjs
-import { createServer } from 'node:http';
+const http = require('http');
 
-const server = createServer((req, res) => {
+http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('Hello World!');
-});
-
-server.listen(8080, '127.0.0.1', () => {
-    console.log('Listening on 127.0.0.1:8080');
-});
+}).listen(8080);
